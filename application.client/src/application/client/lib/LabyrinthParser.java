745771b41a9cf44c4646fad415dcd818c9b4ed0d
LazuLite <ma.sche@hispeed.ch>
2016-12-21 16:15:49 +0100

    Adds labyrinth parser

--- application.client/src/application/client/lib/LabyrinthParser.java
+++ application.client/src/application/client/lib/LabyrinthParser.java
@@ -1,5 +1,30 @@
 package application.client.lib;
 
-public class LabyrinthParser {
+import java.util.Vector;
 
+import javax.swing.text.StyledEditorKit.ForegroundAction;
+import application.client.model.Box;
+
+public class LabyrinthParser(Object serverLabyrinth) {
+	
+	public Vector<Box> parseBoxes() {
+		int size = serverLabyrinth.getSize();
+		
+		Vector<Box> boxes = new Vector<Box>();
+		
+		for(int x=0;x<=size;x++){
+			for (int y=0;y<=size;y++){
+				String status = serverLabyrinth.getBlock(x,y);
+				switch (status) {
+					case 'U':
+						boxes.add(new Box(x, y, false));
+						break;
+					case 'D':
+						boxes.add(new Box(x, y, true));
+						break;
+				}
+			}
+		}
+		return boxes;
+	}
 }
